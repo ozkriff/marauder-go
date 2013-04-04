19823214ee1352d6180234c9d78d1aaae5fa1020
ozkriff <ozkriff@gmail.com>
2013-04-04 15:23:15 +0400

    queue: Simplified comments

--- queue/queue.go
+++ queue/queue.go
@@ -1,6 +1,6 @@
 // See LICENSE file for copyright and license details.
 
-// Package queue реализует чертовски неэффективную очередь.
+// Package queue implements extremle inefficiemnt queue.
 //
 package queue
 
@@ -8,24 +8,20 @@ import (
 	"container/list"
 )
 
-// Queue это простая очередь
 type Queue struct {
 	l list.List
 }
 
-// Push заносит элемент в очередь
 func (self *Queue) Push(value interface{}) {
 	self.l.PushBack(value)
 }
 
-// Pop выталкиевает первый элемент из очереди и возвращает его
 func (self *Queue) Pop() interface{} {
 	tmp := self.l.Front()
 	self.l.Remove(tmp)
 	return tmp.Value
 }
 
-// IsEmpty показывает пуста ли очередь
 func (self *Queue) IsEmpty() bool {
 	return self.l.Len() == 0
 }
